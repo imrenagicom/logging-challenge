318f601d1393ece2df48fbfbcadd8554f8624c7f
Imre Nagi <imre.nagi2812@gmail.com>
2024-01-14 07:16:42 +0100

    feat: add starter code for logging exercise
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>

---
+++ go.mod
@@ -0,0 +1,12 @@
+module logging-challenge
+
+go 1.21.4
+
+require (
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	signal.Notify(ch, syscall.SIGTERM)
+	go func() {
+		oscall := <-ch
+		log.Warn().Msgf("system call:%+v", oscall)
+		cancel()
+	}()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+
+	// start: set up any of your logger configuration here if necessary
+
+	// end: set up any of your logger configuration here
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msg("failed to listen and serve http server")
+		}
+	}()
+	<-ctx.Done()
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Error().Err(err).Msg("failed to shutdown http server gracefully")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	name := r.URL.Query().Get("name")
+	res, err := greeting(ctx, name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(res))
+}
+
+func greeting(ctx context.Context, name string) (string, error) {
+	if len(name) < 5 {
+		return fmt.Sprintf("Hello %s! Your name is to short\n", name), nil
+	}
+	return fmt.Sprintf("Hi %s", name), nil
+}
